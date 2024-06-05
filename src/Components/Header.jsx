c0243c3808a0b055037310d3284de9fc602b3b19
DevFaith <waitherafaith@outlook.com>
2024-06-05 14:20:04 +0300

    * feat: add vite.svg icon to public directory * feat: add App.jsx component * feat: add Header.jsx component * feat: add react.svg icon to assets directory * feat: add index.css file * feat: add main.jsx file * feat: add tailwind.config.js file * feat: add vite.config.js file

---
+++ src/Components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <div>
+        <div>
+            <h1>Big Five</h1>
+        </div>
+        <div>
+            <h1>Home</h1>
+            <h2></h2>
+            <button>Login</button>
+            
+        </div>
+    </div>
+    
+    
+    </>
+
+
+
+
+  )
+}
+
+export default Header
