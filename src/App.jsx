24d631a396e0d32e90c77990af6879b3d76d8c4f
DevFaith <waitherafaith@outlook.com>
2025-04-13 23:14:48 +0300

    feat(App.jsx): implement routing for Home, About, Reports, and Contacts pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
-export default function App() {
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Reports from './Pages/Reports.jsx'
+import Contacts from './Pages/Contacts'
+
+
+
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
+function App() {
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    < >
+
+      <BrowserRouter>
+      <div className='z-10'>
+      <Header />
+      </div>
+      
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/reports' element={<Reports />} />
+          <Route path='/contacts' element={<Contacts />} />
+   
+          
+
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+      
+       
+    </>
+      
+    
   )
 }
+
+export default App
